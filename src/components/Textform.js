0e2f1d0c1051d5a1d3836944a649fcbabdadd8f2
Niharika <niharikathiriveedhi@gmail.com>
2025-04-29 19:23:31 +0530

    fourth commit

--- src/components/Textform.js
+++ src/components/Textform.js
@@ -7,19 +7,34 @@ export default function Textform(props)
         
         setText(newText);
     }
+    const handleLowerClick=()=>{
+        let newText=text.toLowerCase();
+        
+        setText(newText);
+    }
     const handleOnChange=(event)=>{
         console.log("on change");
         setText(event.target.value);
     }
-    const [text,setText]=useState('enter the text here2')
+    const [text,setText]=useState('enter the text')
   return (
     
     <div>
+        <div className="container">
       <div className="mb-3">
   <h3>{props.heading}</h3>
   <textarea className="form-control" value={text} onChange={handleOnChange} id="myBox" rows="8"></textarea>
 </div>
-<button className="btn btn-primary" onClick={handleUpClick}>Convert to Uppercase</button>
+<button className="btn btn-primary mx-2" onClick={handleUpClick}>Convert to Uppercase</button>
+<button className="btn btn-primary mx-2" onClick={handleLowerClick}>Convert to Lowercase</button>
+</div>
+<div className="container my-2">
+<h3>Your Text Summary</h3>
+<b>{(text.split(" ")).length-1} words,{text.length} characters</b>
+<b>Time taken to read entire text {0.008*(text.split(" ").length-1)} minutes</b>
+<h4>Preview</h4>
+<p>{text}</p>
+</div>
     </div>
     
   )
