a71df33b3e6bd7e534be06f17484f90400b16eff
Niharika <niharikathiriveedhi@gmail.com>
2025-04-30 14:55:14 +0530

    sixth commit

--- src/App.js
+++ src/App.js
@@ -2,13 +2,37 @@
 import './App.css';
 import Navbar from './components/Navbar';
 import Textform from './components/Textform';
+import {useState} from 'react'
+import Alert from './components/Alert';
 //import About from './components/About';
 function App() {
+  const[mode,setMode]=useState("light");
+  const [alert,setAlert]=useState(null);
+const showAlert=(message,type)=>{
+setAlert({mess:message,type:type});
+setTimeout(() => {
+  setAlert(null);
+},1500);
+}
+  const toggleMode=()=>{
+    console.log({mode});
+    if(mode === "light"){
+    setMode("dark");
+    document.body.style.backgroundColor="#042743";
+    showAlert("Dark mode enabled","success");
+  }
+    else{
+      setMode("light");
+      document.body.style.backgroundColor="white";
+      showAlert("light mode enabled","success");
+    }
+  }
   return (
    <>
-   <Navbar title="TextUtils"/>
+   <Navbar title="TextUtils" mode={mode} toggleMode={toggleMode}/>
+   <Alert alert={alert}/>
    <div className="container my-3">
-   <Textform heading="enter the text to analyze"/>
+   <Textform heading="enter the text to analyze" mode={mode} showAlert={showAlert}/>
   
    </div>
    </>
--- src/components/Textform.js
+++ src/components/Textform.js
@@ -6,11 +6,13 @@ export default function Textform(props)
         let newText=text.toUpperCase();
         
         setText(newText);
+        props.showAlert("converted to uppercase!","success")
     }
     const handleLowerClick=()=>{
         let newText=text.toLowerCase();
         
         setText(newText);
+        props.showAlert("converted to lowercase!","success")
     }
     const handleOnChange=(event)=>{
         console.log("on change");
@@ -19,6 +21,7 @@ export default function Textform(props)
     const handleClearClick=(event)=>{
     
         setText("");
+        props.showAlert("cleared text","success")
     }
     const speakText=()=>{
         console.log(text);
@@ -30,24 +33,24 @@ export default function Textform(props)
     const [text,setText]=useState('enter the text')
   return (
     
-    <div>
-        <div className="container">
-      <div className="mb-3">
-  <h3>{props.heading}</h3>
-  <textarea className="form-control" value={text} onChange={handleOnChange} id="myBox" rows="8"></textarea>
-</div>
-<button className="btn btn-primary mx-2" onClick={handleUpClick}>Convert to Uppercase</button>
-<button className="btn btn-primary mx-2" onClick={handleLowerClick}>Convert to Lowercase</button>
-<button className="btn btn-primary my-2 mx-2" onClick={handleClearClick}>Clear text</button>
-<button className="btn btn-primary my-2 mx-2" onClick={speakText}>speak text</button>
-</div>
-<div className="container my-2">
-<h3>Your Text Summary</h3>
-<b>{(text.split(" ")).length-1} words,{text.length} characters</b>
-<h4 className="my-3">Preview</h4>
-<p>{text}</p>
-</div>
-    </div>
-    
-  )
-}
+        <div style={{color:props.mode==='light'?'#042743':'white'}}>
+            <div className="container">
+              <div className="mb-3">
+                <h3>{props.heading}</h3>
+                <textarea className="form-control" value={text} onChange={handleOnChange} id="myBox" rows="8" style={{backgroundColor:props.mode==='light'?'white':'grey',color:props.mode==='light'?'#042743':'white'}}></textarea>
+              </div>
+              <button className="btn btn-primary mx-2" onClick={handleUpClick}>Convert to Uppercase</button>
+              <button className="btn btn-primary mx-2" onClick={handleLowerClick}>Convert to Lowercase</button>
+              <button className="btn btn-primary my-2 mx-2" onClick={handleClearClick}>Clear text</button>
+              <button className="btn btn-primary my-2 mx-2" onClick={speakText}>speak text</button>
+            </div>
+            <div className="container my-2">
+              <h3>Your Text Summary</h3>
+              <b>{(text.split(" ")).length-1} words,{text.length} characters</b>
+              <h4 className="my-3">Preview</h4>
+              <p>{text}</p>
+            </div>
+        </div>
+        
+      )
+    }
